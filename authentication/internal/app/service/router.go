69caea8ec50e1300c1e52cf8e7aec775842f030f
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2021-02-28 12:37:29 +0530

    Fix router.go
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

--- authentication/internal/app/service/router.go
+++ authentication/internal/app/service/router.go
@@ -1,13 +1,16 @@
 package service
 
-import (
-	"github.com/gorilla/mux"
-)
+import "github.com/gorilla/mux"
 
 func NewRouter() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/login", HandleLogin).Methods("POST")
-	r.HandleFunc("/register", HandleRegistration).Methods("POST")
-	r.HandleFunc("/validateToken", HandleValidateToken).Methods("POST")
-	return r
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range routes {
+		router.Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+
+	}
+	return router
 }
